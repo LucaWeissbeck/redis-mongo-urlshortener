cb853df140344fe74c1e84b2cb0818f48168b2a5
Luca Weissbeck <62757957+LucaWeissbeck@users.noreply.github.com>
2022-02-21 11:01:03 +0100

    Update db_connection.js

--- services/db_connection.js
+++ services/db_connection.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose")
 const MONGO_CON_STRING = process.env.MONGO_CON_STRING
 
-// redis pw: JWlZPosmOW49saf06HFuONsHeuI5N8DV
-// mongodb+srv://<username>:<password>@systemdesigncluster.smdy5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 mongoose.connect(MONGO_CON_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
